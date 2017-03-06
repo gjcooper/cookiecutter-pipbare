cefcf0472fe9794dee1a25b058670b3cfc826637
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2017-03-06 14:48:34 +1100

    Use reformatted setup.py script to use __about__
    Follows reading on this by:
      https://hynek.me/articles/testing-packaging/
    
    and viewing the package structure on:
      https://github.com/pyca/cryptography

--- {{cookiecutter.app_name}}/setup.py
+++ {{cookiecutter.app_name}}/setup.py
@@ -1,42 +1,48 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
+import codecs
+import os
 import subprocess
 import sys
 
-__version__ = '{{cookiecutter.version}}'
+base_dir = os.path.abspath(os.path.dirname(__file__))
+src_dir = os.path.join(base_dir, 'src')
+# When executing the setup.py, we need to be able to import ourselves, this
+# means that we need to add the src/ directory to the sys.path.
+sys.path.insert(0, src_dir)
+about = {}
+with open(os.path.join(src_dir, '{{cookiecutter.app_name}}', '__about__.py')) as f:
+    exec(f.read(), about)
 
-here = path.abspath(path.dirname(__file__))
 
-pandoc_call = ['pandoc', '--from=markdown', '--to=rst', 'README.md']
-
-try:
-    output = subprocess.run(pandoc_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if output.returncode:
-        print(output.stderr)
-        sys.exit()
-    output = output.stdout
-except AttributeError:
+def genRST():
+    pandoc_call = ['pandoc', '--from=markdown', '--to=rst', 'README.md']
     try:
-        output = subprocess.check_output(pandoc_call)
-    except subprocess.CalledProcessError:
-        sys.exit()
+        output = subprocess.run(pandoc_call, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if output.returncode:
+            print(output.stderr)
+            sys.exit()
+        output = output.stdout
+    except AttributeError:
+        try:
+            output = subprocess.check_output(pandoc_call)
+        except subprocess.CalledProcessError:
+            sys.exit()
+    return output
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with codecs.open(os.path.join(base_dir, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(
-    name='{{cookiecutter.app_name}}',
-    version=__version__,
-    description='{{cookiecutter.project_short_description}}',
-    long_description=output,
-    url='https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}',
-    download_url='https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.app_name}}/tarball/' + __version__,
-    license='GPLv3',
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__summary__'],
+    long_description=genRST(),
+    url=about['__uri__'],
+    license=about['__license__'],
     classifiers=[
       'Development Status :: 3 - Alpha',
       'Intended Audience :: Developers',
@@ -45,8 +51,8 @@ setup(
     keywords='',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
-    author='{{cookiecutter.full_name}}',
+    author=about['__author__'],
     install_requires=install_requires,
     dependency_links=dependency_links,
-    author_email='{{cookiecutter.email}}'
+    author_email=about['__email__']
 )
